a66bb5e20b5eb6fa0e62cff4e08f4a38ac93fe14
Nolan Warner <renrawnalon@gmail.com>
2013-08-01 23:01:50 +0900

    Added .screenrc

--- dot_files/create_links.sh
+++ dot_files/create_links.sh
@@ -36,6 +36,9 @@ function make_directory {
 super_link $HOME/docs/dot_files/dot_bashrc $HOME/.bashrc
 super_link $HOME/docs/dot_files/dot_bash_profile $HOME/.bash_profile
 
+# screenrc files
+super_link $HOME/docs/dot_files/dot_screenrc $HOME/.screenrc
+
 # gitconfig files
 super_link $HOME/docs/dot_files/dot_gitconfig $HOME/.gitconfig
 
